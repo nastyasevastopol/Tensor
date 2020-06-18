2b3e329f16182244e4ee8c6f23ae42b0a10a0b19
nastyasevastopol <arhipova_sevastopol@mail.ru>
2020-06-19 01:21:25 +0300

    commit for adding build

---
+++ build/precache-manifest.9ca27cfa1dc83fdacc1d0b8025719785.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e37c111237df9a3944bc4f876bca83c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "203f9ef7ae70bff4291c",
+    "url": "/static/css/main.a9b1a57c.chunk.css"
+  },
+  {
+    "revision": "94eb7040eea8860a013d",
+    "url": "/static/js/2.febc87e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.febc87e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "203f9ef7ae70bff4291c",
+    "url": "/static/js/main.35737fe5.chunk.js"
+  },
+  {
+    "revision": "ec7688d17208d86382c0",
+    "url": "/static/js/runtime-main.afd9935c.js"
+  }
+]);
